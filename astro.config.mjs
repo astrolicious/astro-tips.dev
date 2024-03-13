a5f322b38c9aba3c1ee5a4609024122db468fc66
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-13 16:16:08 +0100

    feat: basic customization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,22 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: process.env.DEPLOY_URL ?? "https://astro-tips.dev",
+  integrations: [
+    starlight({
+      title: "Astro Tips",
+      social: {
+        github: "https://github.com/astrolicious/astro-tips.dev",
+        discord: "https://astro.build/chat",
+      },
+      sidebar: [
+        {
+          label: "Recipes",
+          autogenerate: { directory: "recipes" },
+        },
+      ],
+    }),
+  ],
 });
