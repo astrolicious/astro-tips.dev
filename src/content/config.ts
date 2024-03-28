a7b5cc5781017c7fdd269252986c5cedbd481282
ktym4a <shoma@ktym4a.me>
2024-03-28 21:51:45 +0700

    Update Astro version validation logic and messages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content'
 import { docsSchema } from '@astrojs/starlight/schema'
-import { validRange } from 'semver'
+import { minVersion, outside, validRange } from 'semver'
+import pkg from 'package.json'
 
 export const collections = {
 	docs: defineCollection({
@@ -9,6 +10,11 @@ export const collections = {
 				astroRange: z
 					.string()
 					.refine(validRange, {message: 'Must be a valid semver range'})
+					.refine((range) => {
+						const astroVersion = minVersion(pkg.dependencies.astro)?.version
+						// check if range is less than the current astro version
+						return astroVersion && !outside(astroVersion, range, '<')
+					}, {message: 'Must be compatible with the current Astro version'})
 					.optional(),
 			}),
 		}),
