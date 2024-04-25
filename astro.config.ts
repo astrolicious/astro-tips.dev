8cf7ac200745e7a782664af6197184a82aa79cef
vasfvitor <vasfvitor@users.noreply.github.com>
2024-04-25 18:07:33 +0000

    [ci] format

--- astro.config.ts
+++ astro.config.ts
@@ -1,41 +1,41 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.DEPLOY_URL ?? "https://astro-tips.dev",
-  redirects: {
-    "/recipes/how-to-add-gsap": "/tips/how-to-add-gsap",
-  },
-  integrations: [
-    starlight({
-      title: "Astro Tips",
-      social: {
-        github: "https://github.com/astrolicious/astro-tips.dev",
-        discord: "https://chat.astrolicious.dev/",
-      },
-      sidebar: [
-        {
-          label: "Home",
-          link: "/",
-        },
-        {
-          label: "Contributing",
-          link: "/contributing/",
-        },
-        {
-          label: "Recipes",
-          autogenerate: { directory: "recipes" },
-        },
-        {
-          label: "Tips",
-          autogenerate: { directory: "tips" },
-        },
-      ],
-      components: {
-        PageTitle: "./src/components/starlight/PageTitle.astro",
-        MarkdownContent: "./src/components/starlight/MarkdownContent.astro",
-      },
-    }),
-  ],
+	site: process.env.DEPLOY_URL ?? 'https://astro-tips.dev',
+	redirects: {
+		'/recipes/how-to-add-gsap': '/tips/how-to-add-gsap',
+	},
+	integrations: [
+		starlight({
+			title: 'Astro Tips',
+			social: {
+				github: 'https://github.com/astrolicious/astro-tips.dev',
+				discord: 'https://chat.astrolicious.dev/',
+			},
+			sidebar: [
+				{
+					label: 'Home',
+					link: '/',
+				},
+				{
+					label: 'Contributing',
+					link: '/contributing/',
+				},
+				{
+					label: 'Recipes',
+					autogenerate: { directory: 'recipes' },
+				},
+				{
+					label: 'Tips',
+					autogenerate: { directory: 'tips' },
+				},
+			],
+			components: {
+				PageTitle: './src/components/starlight/PageTitle.astro',
+				MarkdownContent: './src/components/starlight/MarkdownContent.astro',
+			},
+		}),
+	],
 });
