fdd1f287e95d6b723fbd94163dda8631684f2317
Alexander Niebuhr <alexander@nbhr.io>
2024-04-28 22:27:54 +0200

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import { resolve, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import starlight from "@astrojs/starlight";
 
 import db from "@astrojs/db";
@@ -9,50 +9,59 @@ import db from "@astrojs/db";
 export default defineConfig({
   site: process.env.DEPLOY_URL ?? "https://astro-tips.dev",
   redirects: {
-    "/recipes/how-to-add-gsap": "/tips/how-to-add-gsap"
+    "/recipes/how-to-add-gsap": "/tips/how-to-add-gsap",
   },
   experimental: {
-    contentCollectionJsonSchema: true
+    contentCollectionJsonSchema: true,
   },
-  integrations: [starlight({
-    title: "Astro Tips",
-    social: {
-      github: "https://github.com/astrolicious/astro-tips.dev",
-      discord: "https://chat.astrolicious.dev/"
-    },
-    sidebar: [{
-      label: "Home",
-      link: "/"
-    }, {
-      label: "Contributing",
-      link: "/contributing/"
-    }, {
-      label: "Recipes",
-      autogenerate: {
-        directory: "recipes"
-      }
-    }, {
-      label: "Tips",
-      autogenerate: {
-        directory: "tips"
-      }
-    }, {
-      label: 'Resources',
-      badge: 'New',
-      autogenerate: {
-        directory: 'resources'
-      }
-    }],
-    components: {
-      PageTitle: "./src/components/starlight/PageTitle.astro",
-      MarkdownContent: "./src/components/starlight/MarkdownContent.astro"
-    }
-  }), db()],
+  integrations: [
+    starlight({
+      title: "Astro Tips",
+      social: {
+        github: "https://github.com/astrolicious/astro-tips.dev",
+        discord: "https://chat.astrolicious.dev/",
+      },
+      sidebar: [
+        {
+          label: "Home",
+          link: "/",
+        },
+        {
+          label: "Contributing",
+          link: "/contributing/",
+        },
+        {
+          label: "Recipes",
+          autogenerate: {
+            directory: "recipes",
+          },
+        },
+        {
+          label: "Tips",
+          autogenerate: {
+            directory: "tips",
+          },
+        },
+        {
+          label: "Resources",
+          badge: "New",
+          autogenerate: {
+            directory: "resources",
+          },
+        },
+      ],
+      components: {
+        PageTitle: "./src/components/starlight/PageTitle.astro",
+        MarkdownContent: "./src/components/starlight/MarkdownContent.astro",
+      },
+    }),
+    db(),
+  ],
   vite: {
     resolve: {
       alias: {
-        '~': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
+        "~": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
       },
     },
-  }
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,48 +1,65 @@
-import { defineCollection, z } from 'astro:content'
-import { docsSchema } from '@astrojs/starlight/schema'
-import { minVersion, outside, validRange } from 'semver'
-import pkg from '../../package.json'
+import { db, Authors } from "astro:db";
+import { defineCollection, z } from "astro:content";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { minVersion, outside, validRange } from "semver";
+import pkg from "../../package.json";
 
-const astroVersion = minVersion(pkg.dependencies.astro)?.version
-
+const astroVersion = minVersion(pkg.dependencies.astro)?.version;
+const authors = await db.select({ slug: Authors.slug }).from(Authors);
+const authorConsts = authors.map((author) => author.slug) as [
+	string,
+	...string[]
+];
+console.log("DEBUG", authorConsts);
 const starlightSchema = defineCollection({
 	schema: docsSchema({
 		extend: z.object({
 			astroRange: z
 				.string()
-				.refine(validRange, { message: 'Must be a valid semver range' })
-				.refine((range) => {
-					// check if range is bigger than the current Astro version.
-					return astroVersion && !outside(astroVersion, range, '<')
-				}, { message: `'astroRange' must be compatible with the current released Astro version: '${astroVersion}'` })
+				.refine(validRange, { message: "Must be a valid semver range" })
+				.refine(
+					(range) => {
+						// check if range is bigger than the current Astro version.
+						return astroVersion && !outside(astroVersion, range, "<");
+					},
+					{
+						message: `'astroRange' must be compatible with the current released Astro version: '${astroVersion}'`,
+					}
+				)
 				.optional(),
-			authors: z.array(z.union([z.string(), z.object({
-				/**
-				 * The name of the author.
-				 */
-				name: z.string().min(1),
-				/**
-				 * The title of the author.
-				 */
-				title: z.string().optional(),
-				/**
-				 * The URL or path to the author's picture.
-				 */
-				picture: z.string().optional(),
-				/**
-				 * The URL to the author's website.
-				 */
-				url: z.string().url().optional(),
-			})])).optional(),
+			authors: z
+				.array(
+					z.enum(authorConsts).or(
+						z.object({
+							/**
+							 * The name of the author.
+							 */
+							name: z.string().min(1),
+							/**
+							 * The title of the author.
+							 */
+							title: z.string().optional(),
+							/**
+							 * The URL or path to the author's picture.
+							 */
+							picture: z.string().optional(),
+							/**
+							 * The URL to the author's website.
+							 */
+							url: z.string().url().optional(),
+						})
+					)
+				)
+				.optional(),
 		}),
 	}),
-})
-
+});
 
 const resourcesSchema = defineCollection({
-	type: 'data',
+	type: "data",
 	schema: z.object({
-		category: z.enum(["css",
+		category: z.enum([
+			"css",
 			"auth",
 			"rss",
 			"images",
@@ -52,7 +69,8 @@ const resourcesSchema = defineCollection({
 			"utilities",
 			"animation",
 			"i18n",
-			"db"]),
+			"db",
+		]),
 		title: z.string(),
 		link: z.string(),
 		description: z.string().optional(),
@@ -62,4 +80,4 @@ const resourcesSchema = defineCollection({
 export const collections = {
 	docs: starlightSchema,
 	resources: resourcesSchema,
-}
+};
