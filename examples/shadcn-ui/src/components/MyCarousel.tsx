78461d71edd490a31a34ba5aa026094d0768e224
Oliver Speir <115520730+OliverSpeir@users.noreply.github.com>
2024-11-21 16:51:44 -0700

    Reopen shadcn tip pr (#297)

---
+++ examples/shadcn-ui/src/components/MyCarousel.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent } from "@/components/ui/card"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function MyCarousel() {
+    return (
+        <Carousel
+            opts={{
+                align: "start",
+            }}
+            className="w-full max-w-sm"
+        >
+            <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                        <div className="p-1">
+                            <Card>
+                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                    <span className="text-3xl font-semibold">{index + 1}</span>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    )
+}
