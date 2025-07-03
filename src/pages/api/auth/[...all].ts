c7fd0df607149d08fd61fe9ea5049a84905b994f
Alexander Niebuhr <alexander@nbhr.io>
2025-07-03 03:43:17 +0200

    fix modifying immutable headers

--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -5,6 +5,5 @@ export const prerender = false;
 
 export const ALL: APIRoute = async (ctx) => {
 	const auth = betterAuth(new URL(ctx.request.url).origin, ctx.locals.runtime.env);
-	ctx.request.headers.set('x-forwarded-for', ctx.clientAddress);
 	return auth.handler(ctx.request);
 };
