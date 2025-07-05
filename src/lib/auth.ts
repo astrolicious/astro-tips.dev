b55341b03f2b22b1743579423acf1134f80d3634
Alexander Niebuhr <alexander@nbhr.io>
2025-07-05 10:23:31 +0200

    Revert 6cf8ff741453399950d30bb1e276965dbb8235c2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { betterAuth } from 'better-auth';
-import { admin, oAuthProxy } from 'better-auth/plugins';
+import { admin } from 'better-auth/plugins';
 import { D1Dialect } from 'kysely-d1';
 
 export const auth = (url: string, env: Env) => {
@@ -13,18 +13,11 @@ export const auth = (url: string, env: Env) => {
 		rateLimit: {
 			enabled: true,
 		},
-		plugins: [
-			oAuthProxy({
-				productionURL: 'https://astro-tips.dev',
-				currentURL: url,
-			}),
-			admin(),
-		],
+		plugins: [admin()],
 		socialProviders: {
 			github: {
 				clientId: env.GITHUB_CLIENT_ID,
 				clientSecret: env.GITHUB_CLIENT_SECRET,
-				redirectURI: `https://astro-tips.dev/api/auth/callback/github`,
 			},
 		},
 	});
